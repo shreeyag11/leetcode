646aff9ff2315fc9288c75b89cdf400e2b1ecbf5
Shreeya Gupta <shreeyag11@gmail.com>
2020-05-17 20:37:25 -0400

    easy 728

---
+++ self-dividing-numbers_728.java
@@ -0,0 +1,40 @@
+// A self-dividing number is a number that is divisible by every digit it contains.
+
+// For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+// Also, a self-dividing number is not allowed to contain the digit zero.
+
+// Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+// Example 1:
+// Input: 
+// left = 1, right = 22
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+// Note:
+
+// The boundaries of each input argument are 1 <= left <= right <= 10000 
+
+
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> l = new ArrayList<>();
+        for(int i=left; i<=right; i++)
+            if(selfDivide(i))
+                l.add(i);
+        return l;
+    }
+    public boolean selfDivide(int num){
+        int n = num;
+        while(num>0){
+            int m = num % 10;
+            if(m == 0)
+                return false;
+            if(n % m != 0)
+                return false;
+            num /= 10;
+        }
+        return true;
+    }
+}
+
+
