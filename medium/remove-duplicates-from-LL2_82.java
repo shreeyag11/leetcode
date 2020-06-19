f215bb57ee6da5fe6eb29256b5408d191a13b2c3
Shreeya Gupta <shreeyag11@gmail.com>
2020-06-19 19:35:10 -0400

    using a new listnode to link

--- medium/remove-duplicates-from-LL2_82.java
+++ medium/remove-duplicates-from-LL2_82.java
@@ -11,30 +11,38 @@
 // Input: 1->1->1->2->3
 // Output: 2->3
 
+//  Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null) return null;
+        if (head == null)
+            return null;
         ListNode curr = head;
         ListNode res = new ListNode(0);
         ListNode dummy = res;
         int flag = 0;
-        while(curr != null){
-            while(curr.next!= null &&curr.val ==curr.next.val){
+        while (curr != null) {
+            while (curr.next != null && curr.val == curr.next.val) {
                 curr = curr.next;
-                flag = 1;                
+                flag = 1;
             }
-            if(flag != 1){
+            if (flag != 1) {
                 res.next = curr;
                 res = res.next;
             }
