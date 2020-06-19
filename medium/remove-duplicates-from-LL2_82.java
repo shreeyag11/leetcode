7a484ba091d43b13ec9cd215cbcba404fbe73c9e
Shreeya Gupta <shreeyag11@gmail.com>
2020-06-19 19:34:02 -0400

    using a new listnode to link

---
+++ medium/remove-duplicates-from-LL2_82.java
@@ -0,0 +1,47 @@
+// Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+// Return the linked list sorted as well.
+
+// Example 1:
+
+// Input: 1->2->3->3->4->4->5
+// Output: 1->2->5
+// Example 2:
+
+// Input: 1->1->1->2->3
+// Output: 2->3
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null) return null;
+        ListNode curr = head;
+        ListNode res = new ListNode(0);
+        ListNode dummy = res;
+        int flag = 0;
+        while(curr != null){
+            while(curr.next!= null &&curr.val ==curr.next.val){
+                curr = curr.next;
+                flag = 1;                
+            }
+            if(flag != 1){
+                res.next = curr;
+                res = res.next;
+            }
+            flag = 0;
+            curr = curr.next;
+        }
+        res.next = null;
+        return dummy.next;
+    }
+}
