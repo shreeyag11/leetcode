d881f2f4d8de813ef7de0d082eda8b2d2a7d23e5
Shreeya Gupta <shreeyag11@gmail.com>
2020-07-08 15:58:58 -0400

    using heap and hashmap

--- medium/top-k-freq-elements_347.java
+++ medium/top-k-freq-elements_347.java
@@ -1,8 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 // Given a non-empty array of integers, return the k most frequent elements.
 
 // Example 1:
@@ -19,6 +14,11 @@ import java.util.Queue;
 // Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 // It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
 // You can return the answer in any order.
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class TopKEle {
     public int[] topKFrequent(int[] nums, int k) {
