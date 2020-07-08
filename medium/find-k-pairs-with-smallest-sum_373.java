23ceef6bc3deccca83db395c8324e98a9f267835
Shreeya Gupta <shreeyag11@gmail.com>
2020-07-08 18:34:03 -0400

    using heap

---
+++ medium/find-k-pairs-with-smallest-sum_373.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+// Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+
+// Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+
+// Example 1:
+
+// Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+// Output: [[1,2],[1,4],[1,6]] 
+// Explanation: The first 3 pairs are returned from the sequence: 
+//              [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+// Example 2:
+
+// Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+// Output: [1,1],[1,1]
+// Explanation: The first 2 pairs are returned from the sequence: 
+//              [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+// Example 3:
+
+// Input: nums1 = [1,2], nums2 = [3], k = 3
+// Output: [1,3],[2,3]
+// Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+
+class SmallestPair {
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        List<List<Integer>> l1 = new ArrayList<>();
+        Queue<List<Integer>> pq = new PriorityQueue<>((a, b) -> (b.get(0) + b.get(1)) - (a.get(0) + a.get(1)));
+        for (int i : nums1) {
+            for (int j : nums2) {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(i);
+                temp.add(j);
+                pq.offer(temp);
+                if (pq.size() > k)
+                    pq.poll();
+            }
+        }
+        while (!pq.isEmpty()) {
+            l1.add(pq.poll());
+        }
+        return l1;
+    }
+}
