b843f5eb483733bf6c1ce2b27eac25f4294625d5
Shreeya Gupta <shreeyag11@gmail.com>
2020-07-08 15:58:11 -0400

    using heap and hashmap

---
+++ medium/top-k-freq-elements_347.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+// Given a non-empty array of integers, return the k most frequent elements.
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+// Note:
+
+// You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+// Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+// It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+// You can return the answer in any order.
+
+class TopKEle {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+        Queue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for (Map.Entry<Integer, Integer> count : map.entrySet()) {
+            minHeap.offer(count);
+            if (minHeap.size() > k)
+                minHeap.poll();
+        }
+        int[] res = new int[minHeap.size()];
+        int i = 0;
+        while (!minHeap.isEmpty()) {
+            res[i] = minHeap.poll().getKey();
+            i++;
+        }
+        return res;
+    }
+}
