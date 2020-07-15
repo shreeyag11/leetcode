2f9c67d8f3edfec8040999b0c18a3ab84adb0788
Shreeya Gupta <shreeyag11@gmail.com>
2020-07-15 18:17:12 -0400

    using iterative approach

--- medium/validate-bst_98.java
+++ medium/validate-bst_98.java
@@ -52,3 +52,28 @@ class Solution {
         return true;
     }
 }
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class ValidateBST {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> st = new Stack<>();
+        double inorder = -Double.MAX_VALUE;
+        while (root != null || !st.isEmpty()) {
+            while (root != null) {
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            if (root.val <= inorder)
+                return false;
+            inorder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
