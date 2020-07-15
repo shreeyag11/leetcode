ed1aee7a00a2934d4a41fc337e8be85d9865813a
Shreeya Gupta <shreeyag11@gmail.com>
2020-07-15 00:12:08 -0400

    recursive way

---
+++ medium/binary-tree-preorder-traversal_144.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+// Given a binary tree, return the preorder traversal of its nodes' values.
+
+// Example:
+
+// Input: [1,null,2,3]
+//    1
+//     \
+//      2
+//     /
+//    3
+
+// Output: [1,2,3]
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class PreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null)
+            return result;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left != null)
+                stack.push(node.left);
+        }
+        return result;
+    }
+}
---
+++ medium/invert-binary-tree_226.java
@@ -0,0 +1,43 @@
+// Invert a binary tree.
+
+// Example:
+
+// Input:
+
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// Output:
+
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+// Trivia:
+// This problem was inspired by this original tweet by Max Howell:
+
+// Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return root;
+        if (root.left != null || root.right != null) {
+            TreeNode temp = root.right;
+            root.right = root.left;
+            root.left = temp;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
