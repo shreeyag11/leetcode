6d5497fac3b3cfed59ac4be3ff1852179073067e
Shreeya Gupta <shreeyag11@gmail.com>
2020-07-15 00:27:51 -0400

    iterative approach for the same

--- medium/invert-binary-tree_226.java
+++ medium/invert-binary-tree_226.java
@@ -27,17 +27,44 @@
  * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
  * = left; this.right = right; } }
  */
+// class Solution {
+//     public TreeNode invertTree(TreeNode root) {
+//         if (root == null)
+//             return root;
+//         if (root.left != null || root.right != null) {
+//             TreeNode temp = root.right;
+//             root.right = root.left;
+//             root.left = temp;
+//         }
+//         invertTree(root.left);
+//         invertTree(root.right);
+//         return root;
+//     }
+// }
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null)
             return root;
-        if (root.left != null || root.right != null) {
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while (!st.isEmpty()) {
+            if (root.left != null)
+                st.push(root.left);
+            if (root.right != null)
+                st.push(root.right);
             TreeNode temp = root.right;
             root.right = root.left;
             root.left = temp;
+            root = st.pop();
         }
-        invertTree(root.left);
-        invertTree(root.right);
+
         return root;
     }
 }
