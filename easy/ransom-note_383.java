fb91452b0fc3dad0d2f4cf52e2d261091fbc8063
Shreeya Gupta <shreeyag11@gmail.com>
2020-07-21 17:38:35 -0400

    using char array

---
+++ easy/ransom-note_383.java
@@ -0,0 +1,34 @@
+// Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+// Each letter in the magazine string can only be used once in your ransom note.
+
+// Example 1:
+
+// Input: ransomNote = "a", magazine = "b"
+// Output: false
+// Example 2:
+
+// Input: ransomNote = "aa", magazine = "ab"
+// Output: false
+// Example 3:
+
+// Input: ransomNote = "aa", magazine = "aab"
+// Output: true
+
+// Constraints:
+
+// You may assume that both strings contain only lowercase letters.
+
+class Ransom {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] alpha = new int[26];
+        for (char mag : magazine.toCharArray())
+            alpha[mag - 'a']++;
+        for (char note : ransomNote.toCharArray())
+            alpha[note - 'a']--;
+        for (int i : alpha)
+            if (i < 0)
+                return false;
+        return true;
+    }
+}
