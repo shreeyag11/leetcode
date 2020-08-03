85161660cec1de012288dcd3d89ac80df223d6a2
Shreeya Gupta <shreeyag11@gmail.com>
2020-08-03 11:21:24 -0400

    using stack

---
+++ easy/valid-parenthesis_20.java
@@ -0,0 +1,47 @@
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Note that an empty string is also considered valid.
+
+// Example 1:
+
+// Input: "()"
+// Output: true
+// Example 2:
+
+// Input: "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: "(]"
+// Output: false
+// Example 4:
+
+// Input: "([)]"
+// Output: false
+// Example 5:
+
+// Input: "{[]}"
+// Output: true
+
+class ValidParenthesis {
+    public boolean isValid(String s) {
+        int i = 0;
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                st.push(')');
+            else if (c == '[')
+                st.push(']');
+            else if (c == '{')
+                st.push('}');
+            else if (st.isEmpty() || st.pop() != c)
+                return false;
+        }
+
+        return st.isEmpty();
+    }
+}
