e3ad38908f10a2ed0c9733e41c9a59f7f84192eb
Shreeya Gupta <shreeyag11@gmail.com>
2020-09-23 11:21:49 -0400

    using 2 stacks

---
+++ easy/ImplementStackUsingQueues_225.java
@@ -0,0 +1,70 @@
+// Implement the following operations of a stack using queues.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// empty() -- Return whether the stack is empty.
+// Example:
+
+// MyStack stack = new MyStack();
+
+// stack.push(1);
+// stack.push(2);  
+// stack.top();   // returns 2
+// stack.pop();   // returns 2
+// stack.empty(); // returns false
+// Notes:
+
+// You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+// Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+// You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+
+class MyStack {
+
+    Queue<Integer> q1;    
+    Queue<Integer> q2;
+    int top = -1;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q1 = new LinkedList<>();        
+        q2 = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q1.offer(x);
+        top = x;
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        while(q1.size()>1) {
+            top = q1.poll();
+            q2.offer(top);
+        }
+        int res = q1.poll();
+        while(!q2.isEmpty()) {
+            q1.offer(q2.poll());
+        }
+        return res;
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return top;
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
