d185af73aacf31adc368dd17cc102e09191d2921
Shreeya Gupta <shreeyag11@gmail.com>
2020-11-08 19:59:35 -0500

    brute force

---
+++ Intel/BestTimeToBuyAndSell_121.java
@@ -0,0 +1,14 @@
+package Intel;
+
+public class BestTimeToBuyAndSell_121 {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        for(int i = 0; i < prices.length; i++) {
+            for(int j = i + 1; j < prices.length; j++) {
+                if(prices[j] > prices[i]) {
+                    max = Math.max(max, prices[j] - prices[i]);
+                }
+            }
+        }
+            return max;
+}
