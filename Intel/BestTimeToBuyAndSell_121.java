9c8d46bac18bb821618aa10f46c5abefc5b1e6e0
Shreeya Gupta <shreeyag11@gmail.com>
2020-11-08 20:07:21 -0500

    O(n) time

--- Intel/BestTimeToBuyAndSell_121.java
+++ Intel/BestTimeToBuyAndSell_121.java
@@ -1,14 +1,26 @@
 package Intel;
 
 public class BestTimeToBuyAndSell_121 {
+
     public int maxProfit(int[] prices) {
-        int max = 0;
-        for(int i = 0; i < prices.length; i++) {
-            for(int j = i + 1; j < prices.length; j++) {
-                if(prices[j] > prices[i]) {
-                    max = Math.max(max, prices[j] - prices[i]);
-                }
-            }
+        int min = Integer.MAX_VALUE, maxP = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min)
+                min = prices[i];
+            else if (prices[i] - min > maxP)
+                maxP = prices[i] - min;
         }
-            return max;
+        return maxP;
+    }
+
+    // public int maxProfit(int[] prices) {
+    // int max = 0;
+    // for(int i = 0; i < prices.length; i++) {
+    // for(int j = i + 1; j < prices.length; j++) {
+    // if(prices[j] > prices[i]) {
+    // max = Math.max(max, prices[j] - prices[i]);
+    // }
+    // }
+    // }
+    // return max;
 }
