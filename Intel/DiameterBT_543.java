56db9e491abcef1c59fc2103c08a8895fbbd2fbc
Shreeya Gupta <shreeyag11@gmail.com>
2020-11-11 17:32:38 -0500

    using dfs

---
+++ Intel/DiameterBT_543.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        max = Math.max(max, l + r);
+        return Math.max(l, r) + 1;
+    }
+}
