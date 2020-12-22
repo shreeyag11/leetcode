f1582fca5580e4322c0c49b2697cbbd5dbdedec2
Shreeya Gupta <shreeyag11@gmail.com>
2020-12-22 16:30:23 +0530

    traverse the array for all characters

---
+++ Bloomberg/ValidAnagram.java
@@ -0,0 +1,19 @@
+package Bloomberg;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        int[] alpha = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alpha[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            alpha[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < alpha.length; i++) {
+            if (alpha[i] != 0)
+                return false;
+        }
+        return true;
+    }
+
+}
