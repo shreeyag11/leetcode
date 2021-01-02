e218970c68807e9d6d1b9d72525e531a93c78d96
Shreeya Gupta <shreeyag11@gmail.com>
2021-01-02 17:39:50 +0530

    traverse 2D array

---
+++ easy/richestCustomerWealth.java
@@ -0,0 +1,15 @@
+package easy;
+
+public class richestCustomerWealth {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[0].length; j++) {
+                sum += accounts[i][j];
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+}
