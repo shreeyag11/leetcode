d45460fe24f89e2508bcd61ea6d15c295d1477c3
Shreeya Gupta <shreeyag11@gmail.com>
2021-01-12 17:00:19 +0530

    using math

---
+++ Oracle/RectangleOverlap.java
@@ -0,0 +1,12 @@
+package Oracle;
+
+public class RectangleOverlap {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        if (rec1[0] == rec1[2] || rec1[1] == rec1[3] || rec2[0] == rec2[2] || rec2[1] == rec2[3])
+            return false;
+        return !(rec1[2] <= rec2[0] || // left
+                rec1[3] <= rec2[1] || // bottom
+                rec1[0] >= rec2[2] || // right
+                rec1[1] >= rec2[3]);
+    }
+}
