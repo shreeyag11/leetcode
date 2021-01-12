119e0c4ca2fc96453b6d34dfaff1be528e64d15d
Shreeya Gupta <shreeyag11@gmail.com>
2021-01-12 17:29:03 +0530

    using hashmap

---
+++ Oracle/TwoSumII.java
@@ -0,0 +1,14 @@
+package Oracle;
+
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                return new int[] { map.get(target - numbers[i]), i + 1 };
+            } else
+                map.put(numbers[i], i + 1);
+        }
+        return new int[] { -1, -1 };
+    }
+}
