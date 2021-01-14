5c891da5a60b0dd0a3aeb4ecbad670e4c1d71a07
Shreeya Gupta <shreeyag11@gmail.com>
2021-01-14 16:06:21 +0530

    using hash map and priority queue

---
+++ Oracle/LeastNumberOdUnique.java
@@ -0,0 +1,22 @@
+package Oracle;
+
+public class LeastNumberOdUnique {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        queue.addAll(map.keySet());
+
+        while (!queue.isEmpty() && k > 0) {
+            int key = queue.poll();
+            if (map.get(key) > 1) {
+                map.put(key, map.get(key) - 1);
+                queue.add(key);
+            }
+            k--;
+        }
+        return queue.size();
+    }
+}
