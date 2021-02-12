5f6f7ef200e594bc389f8d21caa80d06a0750d1f
Shreeya Gupta <shreeyag11@gmail.com>
2021-02-12 15:34:07 -0500

    using stack and map

--- easy/next-greater-element-1_496.java
+++ easy/next-greater-element-1_496.java
@@ -35,3 +35,24 @@ class NextGreaterEle1 {
         return res;
     }
 }
+
+// class Solution {
+// public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+// Stack<Integer> st = new Stack<>();
+// HashMap<Integer, Integer> map = new HashMap<>();
+// int[] res = new int[nums1.length];
+// for(int i = 0; i < nums2.length; i++) {
+// while(!st.isEmpty() && nums2[i] > st.peek()) {
+// map.put(st.pop(), nums2[i]);
+// }
+// st.push(nums2[i]);
+// }
+// while(!st.isEmpty()) {
+// map.put(st.pop(), -1);
+// }
+// for(int i = 0; i < nums1.length; i++) {
+// res[i] = map.get(nums1[i]);
+// }
+// return res;
+// }
+// }
