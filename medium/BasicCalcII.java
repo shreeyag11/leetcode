1de03ce82a38be9785a5304de93f30558c4b0937
Shreeya Gupta <shreeyag11@gmail.com>
2021-02-17 17:50:33 -0500

    without using stack

--- medium/BasicCalcII.java
+++ medium/BasicCalcII.java
@@ -33,3 +33,33 @@ public class BasicCalcII {
         return res;
     }
 }
+
+// class Solution {
+// public int calculate(String s) {
+// if(s.length() == 0) return 0;
+// int res = 0;
+// int currNum = 0, lastNum = 0;
+// char op = '+';
+// for(int i = 0; i < s.length(); i++) {
+// char currChar = s.charAt(i);
+// if(Character.isDigit(currChar)) {
+// currNum = (currNum * 10) + (currChar - '0');
+// }
+// if(!Character.isDigit(currChar) && !Character.isWhitespace(currChar) || i ==
+// s.length() - 1) {
+// if(op == '-' || op == '+') {
+// res += lastNum;
+// lastNum = op == '+' ? currNum : - currNum;
+// } else if(op == '*') {
+// lastNum *= currNum;
+// } else if(op == '/') {
+// lastNum /= currNum;
+// }
+// op = currChar;
+// currNum = 0;
+// }
+// }
+// res += lastNum;
+// return res;
+// }
+// }
