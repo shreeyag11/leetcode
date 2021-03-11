1f2084635df08370d395053f8844678f7bd8c0f8
Shreeya Gupta <shreeyag11@gmail.com>
2021-03-11 12:17:06 -0500

    using stack

---
+++ Oracle/EvaluateReversePolish.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+
+            if (!"+-*/".contains(token)) {
+                stack.push(Integer.valueOf(token));
+                continue;
+            }
+
+            int number2 = stack.pop();
+            int number1 = stack.pop();
+
+            int result = 0;
+
+            switch (token) {
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number1 - number2;
+                break;
+            case "*":
+                result = number1 * number2;
+                break;
+            case "/":
+                result = number1 / number2;
+                break;
+            }
+
+            stack.push(result);
+
+        }
+
+        return stack.pop();
+    }
+}
