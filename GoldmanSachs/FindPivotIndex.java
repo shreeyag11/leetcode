91d937794f8fdddd856b0216569e347dd6f9ba38
Shreeya Gupta <shreeyag11@gmail.com>
2021-03-23 16:37:59 -0400

    prefix sum

---
+++ GoldmanSachs/FindPivotIndex.java
@@ -0,0 +1,15 @@
+package GoldmanSachs;
+
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftSum = 0;
+        for (int x : nums)
+            sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftSum == sum - leftSum - nums[i])
+                return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
