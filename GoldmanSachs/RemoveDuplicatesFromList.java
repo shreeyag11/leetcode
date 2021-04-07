8082a613b610c76576541796bbd6147863577503
Shreeya Gupta <shreeyag11@gmail.com>
2021-04-07 16:51:03 -0400

    traversing the list

---
+++ GoldmanSachs/RemoveDuplicatesFromList.java
@@ -0,0 +1,21 @@
+package GoldmanSachs;
+
+public class RemoveDuplicatesFromList {
+    /**
+     * Definition for singly-linked list. public class ListNode { int val; ListNode
+     * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+     * ListNode next) { this.val = val; this.next = next; } }
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+
+        while (temp != null && temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            } else
+                temp = temp.next;
+        }
+
+        return head;
+    }
+}
